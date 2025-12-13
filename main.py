75e87f5b3a084f9732583fc3dcec3fde9c0c0751
nemuyxis <nemu.yxis@gmail.com>
2025-12-12 22:49:28 -0500

    fps update

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    fps = 60
+
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip
+        dt = clock.tick(fps) / 1000
 
 
 
